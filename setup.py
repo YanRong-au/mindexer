b37732b65a4eb5c91dd6d269965b6435eb254576
Thomas Rueckstiess <me@tomr.au>
2025-07-16 13:03:11 +1000

    bump dependency versions to current ones.

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     license="MIT",
     packages=find_packages(),
     scripts=["bin/mindexer"],
-    install_requires=["numpy", "pandas", "pymongo"],
+    install_requires=["numpy==2.3.1", "pandas==2.3.1", "pymongo==4.13.2"],
     zip_safe=False,
 )
